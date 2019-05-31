e843d0484a57a4e61e3d3e0336310c6bc6f3ebc0
Matthew Mata <matthewmata1030@gmail.com>
2019-05-31 15:19:16 -0700

    Added comments

--- server/index.js
+++ server/index.js
@@ -4,15 +4,19 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// Creating server and port number
 const app = express();
 const port = 3000;
 
+// Middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Serves static HTML file
 app.use(express.static(path.join(__dirname, '../client')));
 
+// Proxy router to handle all routes
 app.use('/', proxy({
   target: 'localhost:3000',
   router: {
@@ -23,4 +27,5 @@ app.use('/', proxy({
   }
 }))
 
+// Verifies and sets port on where server is listens at
 app.listen(port, () => console.log(`Listening on proxy port ${port}!`));
